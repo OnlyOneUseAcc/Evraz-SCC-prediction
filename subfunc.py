9939da324089a514170e152e9a0fdc290bb55eda
vasya <vasyayugay99@gmail.com>
2021-03-29 00:03:05 +0300

    update data

--- subfunc.py
+++ subfunc.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def replace_comma(data: pd.DataFrame):
-    data = data.apply(lambda x: x.apply(str).str.replace(',', '.'))
+    data = data.apply(lambda x:  x.apply(str).str.replace(',', '.'))
     data = data.apply(lambda x: pd.to_numeric(x))
     return data
 
