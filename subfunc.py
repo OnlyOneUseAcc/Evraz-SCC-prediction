96a76bdbdf8cfac67a58151d6c3bd1fbe71bdcab
paNoNi <novgorodcevnp@gmail.com>
2021-03-28 20:27:39 +0300

    update drop_columns, date_to_interval

--- subfunc.py
+++ subfunc.py
@@ -9,13 +9,20 @@ def replace_comma(data: pd.DataFrame):
 
 # Время изготовления одного сплава
 def date_to_interval(date_obj: pd.Series):
-    date = pd.to_datetime(date_obj)
+    date = pd.to_datetime(date_obj).copy()
     interval = date.diff()
     interval[1:] = interval[1:].dt.total_seconds() // 60
+    interval[0] = int(interval.mean())
     interval.name = 'Interval'
     return interval
 
 
-def drop_columns(data, thresholder):
+def drop_columns(data: pd.DataFrame, thresholder):
     data_dropped = data[[column for column in data if data[column].count() / len(data) >= thresholder]]
+    dropped_col = list()
+    for col in data.columns:
+        if col not in data_dropped.columns:
+            dropped_col.append(col)
+
+    print(f"Удаленные колонки: {', '.join(dropped_col)}")
     return data_dropped
