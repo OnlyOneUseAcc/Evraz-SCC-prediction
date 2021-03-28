b7075ee8e5c45f86765774a69918bfb6da7f7155
paNoNi <novgorodcevnp@gmail.com>
2021-03-28 18:50:56 +0300

    add date_to_interval

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
+import pandas as pd
+
 def replace():
     pass
 
-def date_to_interval(data):
-    pass
+
+# Время изготовления одного сплава
+def date_to_interval(date_obj: pd.Series):
+    date = pd.to_datetime(date_obj)
+    interval = date.diff()
+    interval[1:] = interval[1:].dt.total_seconds() // 60
+    interval.name = 'Interval'
+    return interval
