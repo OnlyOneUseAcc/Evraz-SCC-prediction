0d1bde3563963125128e861ba133597f087963b7
OnlyOneUseAcc <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-03-28 18:38:06 +0300

    replace commas func

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
-def replace():
-    pass
+import pandas as pd
+
+
+def replace_comma(data: pd.DataFrame):
+    data = data.apply(lambda x: x.apply(str).str.replace(',', '.'))
+    data = data.apply(lambda x: pd.to_numeric(x))
+    return data
+
 
 def date_to_interval(data):
     pass
